44976881a9de9ae8f11844b7d4c68ff3a4344f3e
Sharoz Ahmed <sharoz.inocent@gmail.com>
2022-05-04 17:21:58 +0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Header } from "./Components";
+
+function App() {
+  return (
+    <div className="w-screen h-auto flex flex-col">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
